ccfcb8e411982b8ff44747a04a9f05dc6bee8a2b
paldy <58873376+paldy@users.noreply.github.com>
2020-01-06 10:30:07 +0300

    Create re223.py

---
+++ re223.py
@@ -0,0 +1,24 @@
+import requests
+import re
+
+line1 = 'https://stepic.org/media/attachments/lesson/24472/sample1.html'
+line2 = 'https://stepic.org/media/attachments/lesson/24472/sample2.html'
+#line1 = input()
+#line2 = input()
+
+
+pattern = r'<a\s+(?:[^>]*?\s+)?href="(https?:.*?)"'
+allinks = []
+
+res_html = requests.get(line1)
+fromline1 = re.findall(pattern, res_html.text)
+
+for i in fromline1:
+    res = requests.get(i)
+    froml = re.findall(pattern, res.text)
+    allinks = allinks + froml
+
+if line2 not in allinks: 
+    print('No')
+else:
+    print('Yes')
